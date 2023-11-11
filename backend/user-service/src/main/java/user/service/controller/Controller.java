ed92af30e586661677f75667c3732b245386404c
marcinele <marcinten@gmail.com>
2023-11-11 15:06:24 +0100

    Logging for backend: - set up config file for logback - configured logging for backend services - added necessary code adjustments

--- backend/user-service/src/main/java/user/service/controller/Controller.java
+++ backend/user-service/src/main/java/user/service/controller/Controller.java
@@ -1,12 +1,18 @@
 package user.service.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    static Logger log = LoggerFactory.getLogger(Controller.class);
+
     @GetMapping(path = "test")
     public String test() {
+        log.info("test");
         return "siema";
     }
 }
