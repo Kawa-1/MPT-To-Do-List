d495a283bc0544d60c1c4aae0514e066415cfb07
marcinele <marcinten@gmail.com>
2023-11-11 16:28:32 +0100

    Logging for backend: - added logging config to discovery and gateway service - adjusted log filename pattern

--- backend/user-service/src/main/java/user/service/UserServiceApp.java
+++ backend/user-service/src/main/java/user/service/UserServiceApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApp {
     public static void main(String[] args) {
-        System.setProperty("service-name", "user_service");
         SpringApplication.run(UserServiceApp.class, args);
     }
 }
