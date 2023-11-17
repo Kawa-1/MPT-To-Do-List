cacda095af99378a366f2430290303149746a2f3
Wiktor Tekiela <56120539+abcdrefg@users.noreply.github.com>
2023-11-17 23:48:09 +0100

    Added newest version of bootstrap and angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mpt-to-do-list';
