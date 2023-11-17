bc3be3ff8257515405f2862c335be85ad78e5d81
Wiktor Tekiela <56120539+abcdrefg@users.noreply.github.com>
2023-11-17 23:27:52 +0100

    rebuilt project

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [
-  { path: '', component: LoginPageComponent },
-];
-
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginPageComponent,
-    MainLayoutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule,
     AppRoutingModule
   ],
-  providers: []
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
