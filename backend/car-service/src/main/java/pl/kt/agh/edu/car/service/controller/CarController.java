2728a9af57451bc0843d7958ff561577afb87643
“Marcin <marcin.karcz374@interia.pl>
2023-12-16 15:13:22 +0100

    + request body annotation

--- backend/car-service/src/main/java/pl/kt/agh/edu/car/service/controller/CarController.java
+++ backend/car-service/src/main/java/pl/kt/agh/edu/car/service/controller/CarController.java
@@ -3,6 +3,7 @@ package pl.kt.agh.edu.car.service.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.kt.agh.edu.car.service.service.CarService;
 import pl.kt.agh.model.dto.CarDTO;
@@ -20,7 +21,7 @@ public class CarController {
     }
 
     @PostMapping("/car/create")
-    public CarDTO createCar(CarDTO carDTO) {
+    public CarDTO createCar(@RequestBody CarDTO carDTO) {
         return carService.createCar(carDTO);
     }
 
